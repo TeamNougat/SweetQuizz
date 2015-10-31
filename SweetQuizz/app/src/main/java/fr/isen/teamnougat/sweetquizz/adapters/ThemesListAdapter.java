f8fcf990564c81e36bca2bb439a82c0b4b2b07a3
dziter <baptiste@macbook-pro-de-baptiste.home>
2015-10-31 02:39:33 +0100

    Click on cell, ripple effect

--- SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/adapters/ThemesListAdapter.java
+++ SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/adapters/ThemesListAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,8 +19,8 @@ import fr.isen.teamnougat.sweetquizz.model.PlaceTheme;
 public class ThemesListAdapter extends RecyclerView.Adapter<ThemesListAdapter.ViewHolder> {
 
     Context mContext;
+    OnItemClickListener mItemClickListener;
 
-    // 2
     public ThemesListAdapter(Context context) {
         this.mContext = context;
     }
@@ -42,8 +43,12 @@ public class ThemesListAdapter extends RecyclerView.Adapter<ThemesListAdapter.Vi
         return new PlaceTheme().placeList().size();
     }
 
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+
     // 3
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public LinearLayout placeHolder;
         public LinearLayout placeNameHolder;
         public TextView placeName;
@@ -55,6 +60,18 @@ public class ThemesListAdapter extends RecyclerView.Adapter<ThemesListAdapter.Vi
             placeName = (TextView) itemView.findViewById(R.id.placeName);
             placeNameHolder = (LinearLayout) itemView.findViewById(R.id.placeNameHolder);
             placeImage = (ImageView) itemView.findViewById(R.id.placeImage);
+            placeHolder.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            if (mItemClickListener != null) {
+                mItemClickListener.onItemClick(itemView, getPosition());
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
