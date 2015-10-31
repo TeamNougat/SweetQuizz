15065ea50d47adbe84a1aba59673b1b191e61cac
dziter <baptiste@macbook-pro-de-baptiste.home>
2015-10-31 02:09:48 +0100

    NavigationDrawerHelper click

---
+++ SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/activities/FragmentHome.java
@@ -0,0 +1,44 @@
+package fr.isen.teamnougat.sweetquizz.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.isen.teamnougat.sweetquizz.R;
+
+
+public class FragmentHome extends Fragment {
+
+    public FragmentHome() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/activities/MainActivity.java
+++ SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package fr.isen.teamnougat.sweetquizz.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,7 +14,7 @@ import android.view.View;
 import fr.isen.teamnougat.sweetquizz.R;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
-
+    private static String TAG = MainActivity.class.getSimpleName();
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
 
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         drawerFragment = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+
+        displayView(0);
     }
 
 
@@ -55,6 +60,35 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
 
     @Override
     public void onDrawerItemSelected(View view, int position) {
-        
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new FragmentHome();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                //
+                break;
+            case 2:
+                //
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
