ae92bbcd7620ec5d2d826d9b1f376f9d4855bcb4
Dhawos <louis.gerard@isen-lille.fr>
2015-11-09 00:01:28 +0100

    Bug fixe : Timer now correctly stops at the end of a quizz

--- SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/activities/QuizzActivity.java
+++ SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/activities/QuizzActivity.java
@@ -77,33 +77,13 @@ public class QuizzActivity extends AppCompatActivity implements TimeListener,Que
     }
 
     public void loadEndQuizz(){
+        myQuizz.getTimer().stopQuizzTimer();
         Result result = myQuizz.calculateResult();
         Intent intent = new Intent(this,QuizzFinishedActivity.class);
         intent.putExtra("result",result);
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(fr.isen.teamnougat.sweetquizz.R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-/*        //noinspection SimplifiableIfStatement
-        if (id == fr.isen.teamnougat.sweetquizz.R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onTimeChanged() {
