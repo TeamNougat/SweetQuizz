ce183afbd9af694892c7bfdc88491973c19c0362
nathanator1992 <nathan.vaksmann@isen-lille.fr>
2015-11-12 23:55:04 +0100

    Update ResultFragment, now we can choose multiple answer.

--- SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/adapters/ResultAdapter.java
+++ SweetQuizz/app/src/main/java/fr/isen/teamnougat/sweetquizz/adapters/ResultAdapter.java
@@ -8,9 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import fr.isen.teamnougat.sweetquizz.R;
@@ -23,20 +26,17 @@ import fr.isen.teamnougat.sweetquizz.model.quizz.Question;
  */
 public class ResultAdapter extends BaseAdapter {
     private List<Question> question;
-    private ListView listView;
-    private FloatingActionButton imageButton;
-    private Context context;
     private LayoutInflater layoutInflater;
+    private Context context;
+    private Boolean isSet;
 
-    public void setImageButton(FloatingActionButton imageButton) {
-        this.imageButton = imageButton;
-    }
 
     public ResultAdapter(List<Question> myList) {
         this.question = myList;
-        layoutInflater = LayoutInflater.from(SweetQuizz.getAppContext());
+        isSet = false;
+        context = SweetQuizz.getAppContext();
+        layoutInflater = LayoutInflater.from(context);
     }
-
 
 
     @Override
@@ -59,61 +59,59 @@ public class ResultAdapter extends BaseAdapter {
         MyViewHolder mViewHolder;
 
         if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.result_layout,null);
+            convertView = layoutInflater.inflate(R.layout.result_layout, null);
             mViewHolder = new MyViewHolder(convertView);
             convertView.setTag(mViewHolder);
         } else {
             mViewHolder = (MyViewHolder) convertView.getTag();
         }
-
-
+        int index = position + 1;
+        mViewHolder.question_button.setText("Question " + index);
+        mViewHolder.question_button.setWidth(parent.getWidth());
+        mViewHolder.question_button.setVisibility(View.VISIBLE);
+        Iterator<Button> it = mViewHolder.listButton.iterator();
+        int i = 0;
         for (Answer a : question.get(position).getAnswers()) {
             if (a.isChecked()) {
-                mViewHolder.answerB.setText(a.getText());
+                mViewHolder.listButton.get(i).setText(a.getText());
+                mViewHolder.listButton.get(i).setWidth(parent.getWidth());
+                mViewHolder.listButton.get(i).setVisibility(View.VISIBLE);
+                if (a.isTrue()) {
+                    mViewHolder.listButton.get(i).setBackgroundColor(Color.GREEN);
+                    i++;
+                } else if (!a.isTrue()) {
+                    mViewHolder.listButton.get(i).setBackgroundColor(Color.RED);
+                    i++;
+                }
             }
         }
-        if (question.get(position).checkAnswer()) {
-
-            mViewHolder.answerB.setBackgroundColor(Color.GREEN);
-        }
-        else {
-
-            mViewHolder.answerB.setBackgroundColor(Color.RED);
-        }
-
-        mViewHolder.answerB.setTextColor(Color.BLACK);
-
         return convertView;
-        /*convertView= (View) layoutInflater.inflate(R.layout.result_layout);
-        /*Button questions = (Button) convertView.findViewById(R.id.result_layout_question_button);
-        Button answer = (Button) convertView.findViewById(R.id.result_layout_button_answer_button);
-        Button questions = new Button(SweetQuizz.getAppContext());
-
-        questions.setText(question.get(position).getText());
-        for (Answer a : question.get(position).getAnswers()) {
-            if (a.isChecked()) {
-                questions.setText(a.getText());
-            }
-        }
-        if (question.get(position).checkAnswer()) {
-            questions.setBackgroundColor(Color.GREEN);
-            questions.setBackgroundColor(Color.GREEN);
-        }
-        else {
-            questions.setBackgroundColor(Color.RED);
-            questions.setBackgroundColor(Color.RED);
-        }
-        questions.setTextColor(Color.WHITE);
-        questions.setTextColor(Color.BLACK);
-        return questions;*/
     }
 
     private class MyViewHolder {
-        Button answerB;
+        public LinearLayout linearLayout;
+        public Button question_button;
+        public Button answer1, answer2, answer3, answer4, answer5, answer6;
+        public LinkedList<Button> listButton;
+
 
 
         public MyViewHolder(View item) {
-            answerB = (Button) item.findViewById(R.id.result_layout_button_answer_button);
+            listButton = new LinkedList<>();
+            question_button = (Button) item.findViewById(R.id.question_button);
+            answer1 = (Button) item.findViewById(R.id.answer_button1);
+            answer2 = (Button) item.findViewById(R.id.answer_button2);
+            answer3 = (Button) item.findViewById(R.id.answer_button3);
+            answer4 = (Button) item.findViewById(R.id.answer_button4);
+            answer5 = (Button) item.findViewById(R.id.answer_button5);
+            listButton.add(0, answer1);
+            listButton.add(1,answer2);
+            listButton.add(2,answer3);
+            listButton.add(3,answer4);
+            listButton.add(4,answer5);
+            listButton.add(5,answer6);
+
+
 
         }
     }
